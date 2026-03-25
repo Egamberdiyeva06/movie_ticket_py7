934abc01616775c3b49765bec556dda206829920
Durdona Egamberdiyeva <durdonaegamberdiyeva36@gmail.com>
2026-03-25 20:50:21 +0500

    api, main, schemas.py qo'shildi

---
+++ schemas.py
@@ -0,0 +1,24 @@
+from pydantic import BaseModel, Field
+
+class TicketCreate(BaseModel):
+    movie_name: str = Field(max_length=200)
+    seat_number: int = Field(ge=1, le=50)
+    customer_name: str = Field(max_length=100)
+    is_vip: bool = False
+
+
+class TicketOut(BaseModel):
+    ticket_id: int = Field(ge=1)
+    movie_name: str = Field(max_length=200)
+    seat_number: int = Field(ge=1, le=50)
+    customer_name: str = Field(max_length=100)
+    is_vip: bool = False
+    price: float = Field(default=0)
+
+class TicketGet(BaseModel):
+    ticket_id: int
+    movie_name: str
+    seat_number: int
+    customer_name: str
+    is_vip: bool
+    price: float
