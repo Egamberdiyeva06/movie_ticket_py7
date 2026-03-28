5b920818ee2a3bedd6c543d92e77563d112fa538
Durdona Egamberdiyeva <durdonaegamberdiyeva36@gmail.com>
2026-03-28 12:41:29 +0500

    andpointlarni qo'shdim

--- api.py
+++ api.py
@@ -14,8 +14,8 @@ def create_ticket(ticket_in: TicketCreate):
     for sold_ticket in tickets:
         if (sold_ticket["seat_number"] == ticket_in.seat_number and
             sold_ticket["movie_name"] == ticket_in.movie_name):
-            return HTTPException(status_code=404, detail="Bu bilet allaqachon sotilgan.")
-        
+            raise HTTPException(status_code=404, detail="Bu bilet allaqachon sotilgan.")
+                
     price = 80000 if ticket_in.is_vip else 40000
 
     new_ticket = {
@@ -31,9 +31,9 @@ def create_ticket(ticket_in: TicketCreate):
 
     return new_ticket
 
-# @api_router.get('/', response_model=list[TicketGet])
-# def get_ticket():
-#     return tickets
+@api_router.get('/', response_model=list[TicketGet])
+def get_ticket():
+    return tickets
 
 @api_router.get("/{ticket_id}", response_model=TicketGet)
 def get_ticket_by_id(ticket_id: int):
@@ -41,3 +41,28 @@ def get_ticket_by_id(ticket_id: int):
         if ticket["ticket_id"] == ticket_id:
             return ticket
     raise HTTPException(status_code=404, detail="Topilmadi")
+
+@api_router.put("/{ticket_id}", response_model=TicketOut)
+def update_ticket(ticket_id: int, ticket_in: TicketCreate):
+    for i in tickets:
+        if i["ticket_id"] == ticket_id:
+            i["customer_name"] = ticket_in.customer_name
+            i["seat_number"] = ticket_in.seat_number
+            i["movie_name"] = ticket_in.movie_name
+            i["is_vip"] = ticket_in.is_vip
+
+            i["price"] = 80000 if ticket_in.is_vip else 40000
+
+            return i
+        
+    raise HTTPException(status_code=404, detail="Chipta topilmadi!")
+
+
+@api_router.delete("/{ticket_id}")
+def delete_ticket(ticket_id: int):
+    for i, j in enumerate(tickets):
+        if j["ticket_id"] == ticket_id:
+            tickets.pop(i)
+            return {"massage" : "Chipta o'chirildi!"}
+        
+    raise HTTPException(status_code=404, detail="Chipta topilmadi!")
